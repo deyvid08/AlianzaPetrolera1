859f47292ad852f230cec387bc2969fed8a70c13
deyvidfmartinez@funsanmateo.edu.co <deyvidfmartinez@funsanmateo.edu.co>
2019-03-18 16:39:17 -0500

    Funcionamiento roles comentado en Home controller

--- AlianzaPetrolera1/AlianzaPetrolera/Controllers/HomeController.cs
+++ AlianzaPetrolera1/AlianzaPetrolera/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AlianzaPetrolera.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,9 +13,35 @@ namespace AlianzaPetrolera.Controllers
     {
         public ActionResult Index()
         {
+            //    if (User.Identity.IsAuthenticated)
+            //    {
+
+
+            //        using (ApplicationDbContext db = new ApplicationDbContext())
+            //        {
+            //            var userId = User.Identity.GetUserId();
+
+            //            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+
+            //            //Crear Role
+
+
+            //            var resultado = roleManager.Create(new IdentityRole("Padre"));
+
+            //            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+            //            //Agregar Usuario A Role
+
+            //            resultado = userManager.AddToRole(userId, "Padre");
+
+            //            //Usuario Esta En Rol?
+            //            var usuarioEstaEnRol = userManager.IsInRole(userId, "Padre");
+
+            //        }
+            //    }
             return View();
         }
-
+        //[Authorize(Users = "Dmartinez")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +49,7 @@ namespace AlianzaPetrolera.Controllers
             return View();
         }
 
+        //[Authorize(Roles = "Padre")]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
