4e7d1ed163829c4d3f755d384acba78ebdc68166
deyvidfmartinez@funsanmateo.edu.co <deyvidfmartinez@funsanmateo.edu.co>
2019-07-18 15:38:49 -0500

    Recibo funcional con calculos automaticos

---
+++ AlianzaPetrolera1/AlianzaPetrolera/Scripts/calculo.js
@@ -0,0 +1,38 @@
+﻿function sumar() {
+
+    var matricula = $(Matricula).val();
+    var poliza = $(Poliza).val();
+    var uniforme = $(Uniforme).val();
+    var mensualidad = $(Mensualidad).val();
+    var descmatri = $(DescMatri).val();
+    var descpoli = $(DescPoli).val();
+    var descunif = $(DescUnif).val();
+    var descmensu = $(DescMensu).val();
+    var total = 0;
+
+    var calmatri
+    var finmatri
+    var calpoli
+    var finpoli
+    var calunif
+    var finunif
+    var calmensu
+    var finmensu
+
+    calmatri = ((matricula * descmatri) / 100);
+    finmatri = (matricula - calmatri);
+
+    calpoli = ((poliza * descpoli) / 100);
+    finpoli = (poliza - calpoli);
+
+    calunif = ((uniforme * descunif) / 100);
+    finunif = (uniforme - calunif);
+
+    calmensu = ((mensualidad * descmensu) / 100);
+    finmensu = (mensualidad - calmensu);
+
+    total = finmatri + finpoli + finunif + finmensu;
+
+
+    $(Total).val(total);
+}
